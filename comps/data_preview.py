32f647e3276daaff633dafbc70e4bce4bdce38a9
Nadya Moisseeva <nmoisseeva@eos.ubc.ca>
2016-07-21 19:04:07 -0700

    edits for sensitivity and comps scripts

--- comps/data_preview.py
+++ comps/data_preview.py
@@ -3,30 +3,48 @@ from scipy.io import netcdf
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import netcdf
+from scipy import interpolate
 
-wrfdata = '/Users/nadya2/data/plume/comps/part1/wrfout_d01_0000-01-01_00:00:00'
-vars_4d = ['U','V','QVAPOR']
-vars_3d = ['HGT','GRNHFX','AVG_FUEL_FRAC','GRNQFX','PBLH']
+wrfdata = '/Users/nadya2/data/plume/comps/part1/wrfout_2'
+vert_levels = np.arange(0,3000,100)
+
+# vars_4d = ['U','V','QVAPOR']
+# vars_3d = ['HGT','GRNHFX','AVG_FUEL_FRAC','GRNQFX']
 
 print('Extracting NetCDF data from %s ' %wrfdata)
-nc_data = netcdf.netcdf_file(wrfdata, mode ='a')             
-fc_data = {}
+nc_data = netcdf.netcdf_file(wrfdata, mode ='r')             
+# fc_data = {}
 
-#copy variables of interest into a dictionary
-for var4 in vars_4d:
-	print('......... %s' %var4)
-	fc_data[var4] = nc_data.variables[var4][:,:,:,:]
+# #copy variables of interest into a dictionary
+# for var4 in vars_4d:
+# 	print('......... %s' %var4)
+# 	fc_data[var4] = nc_data.variables[var4][:,:,:,:]
 
-for var3 in vars_3d:
-    print('......... %s' %var3)
-    fc_data[var3] = nc_data.variables[var3][:,:,:]
+# for var3 in vars_3d:
+#     print('......... %s' %var3)
+#     fc_data[var3] = nc_data.variables[var3][:,:,:]
 
-print('Appending netcdf file with level height data')
+
 #add level height data
+print('Calculating vertical level heights for the domain')
 nc_data.createVariable('LVLHGT', float, ('Time', 'bottom_top_stag', 'south_north', 'west_east'))
 lvlhgt = (nc_data.variables['PH'][:,:,:,:] + nc_data.variables['PHB'][:,:,:,:]) / 9.81
 nc_data.variables['LVLHGT'] = lvlhgt
-nc_data.sync()
+dims = np.shape(lvlhgt)
 
-nc_data.close()
-# plt.imshow(fc_data['V'][100,:,75,:], origin='lower')
+
+#interpolate to vertical height levels
+print('Performing vertical interpolation of model data')
+T_agl = np.empty((dims[0],len(vert_levels),dims[2],dims[3]))
+for nTime in range(dims[0]):
+	print('.....timestep since simulation start: %smin' %nc_data.variables['XTIME'][nTime])
+	for nY in range(dims[2]):
+		for nX in range(dims[3]):
+			eta =  lvlhgt[nTime,:-1,nY,nX]
+			interp_func = interpolate.interp1d(eta, nc_data.variables['T'][nTime,:,nY,nX],\
+						 'cubic', bounds_error=False, fill_value=np.nan)
+			T_agl[nTime,:,nY,nX] = interp_func(vert_levels)
+
+# nc_data.close()
+plt.pcolormesh(T_agl[120,:,75,:])
+plt.show()
--- sensitivity/plume_hist.py
+++ sensitivity/plume_hist.py
@@ -3,6 +3,7 @@
 # Creates the following figures:
 #			- subplots of individual plumes (horizontally integrated)
 #			- scatter plot of sensitivity
+# 			- animation of cross-wind integrated mixing ratio
 #===============================================================
 
 
@@ -11,6 +12,7 @@ tag = 'v'
 test_range = range(2,14,2)
 data_path = '/Users/nadya2/data/plume/sensitivity/'
 fig_dir = '/Users/nadya2/code/plume/figs/sensitivity/'
+npy_dir = '/Users/nadya2/code/plume/npy/'
 plot_dims = [2,3]
 
 #----------------------------end of input------------------------
@@ -20,13 +22,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import netcdf
 
+#create a storage dictionary
+plume_dict = {}
+plume_dict['var'] = sens_var
+plume_dict['meta'] = 'Values correspond to water vapour mixing ratio anomaly (QVAPOR)'
+plume_dict['range'] = []
+plume_dict['data'] = {}
 
+
+#fill data dictionary, plot column horizontally integrated timeseries
 print('Performing analysis of individual runs for: %s' %sens_var)
 row_int_q = []
 fig, axes = plt.subplots(nrows=plot_dims[0], ncols=plot_dims[1],figsize=(20,10))
 for nTest, test in enumerate(test_range):
-	wrfdata = data_path + sens_var + '/wrfout_%s%s' %(test,tag)
-
+	suffix = '%s%s' %(test,tag)
+	wrfdata = data_path + sens_var + '/wrfout_%s' %suffix
 	print('.....Extracting NetCDF data from %s ' %wrfdata)
 	nc_data = netcdf.netcdf_file(wrfdata, 'r')             
 
@@ -34,12 +44,19 @@ for nTest, test in enumerate(test_range):
 	lvlhgt = (nc_data.variables['PH'][:,:,:,:] + nc_data.variables['PHB'][:,:,:,:]) / 9.81
 	height_vector = np.mean(np.mean(np.mean(lvlhgt,3),2),0)
 
-	#calculate vapor mixing ratio anomaly
-	anom = np.nansum(np.nansum(nc_data.variables['QVAPOR'][:,:,:,:] - nc_data.variables['QVAPOR'][0,:,:,:],3),2)
-	row_int_q.append(anom)
+	#calculate vapor mixing ratio anomaly and store it for future use
+	anom = nc_data.variables['QVAPOR'][:,:,:,:] - nc_data.variables['QVAPOR'][0,:,:,:]
+	plume_dict['data'][suffix] = anom
+	plume_dict['range'].append(suffix)
+
+	#integrate horizontally to produce single vertical column
+	column = np.nansum(np.nansum(anom,3),2)
+	row_int_q.append(column)
+
+	#plotting
 	plt.subplot(2,3,nTest+1)
 	im = plt.pcolormesh(row_int_q[nTest].T, vmin=-0.1, vmax=0.3)
-	plt.title('%s: %s%s' %(sens_var,test,tag), fontsize = 12)
+	plt.title('%s: %s' %(sens_var,suffix), fontsize = 12)
 	plt.xlabel('time since start [min]')
 	plt.ylabel('height [m]')
 	plt.ylim([0,nc_data.dimensions['bottom_top']])
@@ -55,6 +72,11 @@ plt.close()
 print('.....QVAPOR departure (individual subplots) saved as: %s' %fig_path)
 nc_data.close()
 
+print('Saving data dictionary in %s' %npy_dir)
+np.save(npy_dir + 'plume_dict_%s.npy' %sens_var, plume_dict)
+
+
+#sensitivity scatter plots
 print('Determining plume rise sensitivity')
 qvap_array = np.array(row_int_q)
 ave_idx = np.argmax(np.mean(qvap_array,1),1)
@@ -73,3 +95,6 @@ fig_path = fig_dir + sens_var + '/QVAPOR_scatter.pdf'
 plt.savefig(fig_path)
 plt.close()
 print('.....QVAPOR sensitivity scatter saved as: %s' %fig_path)
+
+#cross-wind integrated vapour animations
+print('Creating an animated plot of cross-wind contours')
