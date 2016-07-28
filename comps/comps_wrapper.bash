6e29744a9cab2351378e1c8cbc7e87117865997d
Nadya Moisseeva <nmoisseeva@eos.ubc.ca>
2016-07-28 16:38:18 -0700

    Jul 28, 2016 - comps codes, update to RxCadre case

---
+++ comps/comps_wrapper.bash
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+
+#comps_wrapper.bash
+#=============================
+#this script is a wrapper for performing performing Rolands comps subsections
+
+#input-----------------------------
+part="A"
+section=(a b c)
+input_dir="/Users/nadya2/code/plume/comps/"
+data_dir="/Users/nadya2/data/plume/comps/"
+
+#----------------------------------
+
+echo "==========================================================="
+echo "Running comps section bundle: $part"
+echo "==========================================================="
+
+#local dir
+local=${pwd}
+
+
+
+#loop through all subsections
+for nRun in "${section[@]}"
+do
+    #switch into wrf-fire directory
+    cd ~/Applications/WRFV3/test/em_fire/
+
+    #clean up original settings files
+    rm input_sounding
+    rm namelist.input
+    rm namelist.fire
+
+    echo "..... subsection $nRun"
+    #symlink the proper setting files
+    ln -s ${input_dir}${part}/namelist.fire ./
+    ln -s ${input_dir}${part}/namelist.input ./
+    ln -s ${input_dir}${part}/input\_sounding\_${nRun} ./input\_sounding
+    echo "Running WRF for case: $nRun"
+    ./ideal.exe
+    echo ".....IDEAL.EXE complete"
+    ./wrf.exe 
+    echo ".....WRF.EXE complete: $date"
+
+    #rename and move the results to a data directory
+    mkdir -p ${data_dir}${part}/ 
+    mv wrfout\_d01\_0000\-01\-01\_00:00:00 ${data_dir}${part}/wrfout\_$nRun
+
+    #run interpolation code
+    echo "Running NCL for data interpolation: $nRun"
+    cd ${input_dir}${part}/
+    mkdir -p interp
+    ln -s  ${data_dir}${part}/wrfout\_$nRun ./interp/wrfout\_temp
+    ncl63 data_prep.ncl 
+    mv intperp\_wrf.nc ./intperp/intperp\_wrf\_${nRun}
+    
+done
+
+
+
+# prep for VAPOR
+# #move to data directory
+# cd ${data_dir}${part}
+
+# for nRun in "${section[@]}"
+# do
+#     #create vdf files for VAPOR
+#     ncdfvdfcreate -timedims Time -stagdims bottom_top_stag:south_north_stag:west_east_stag -vars U:V:W:T:QVAPOR:GRNHFX:AVG_FUEL_FRAC wrfout\_$nRun $testvar\_$nRun.vdf
+#     ncdf2vdf -timedims Time -stagdims bottom_top_stag:south_north_stag:west_east_stag -vars U:V:W:T:QVAPOR:GRNHFX:AVG_FUEL_FRAC wrfout\_$nRun $testvar\_$nRun.vdf
+# done
+
+
+cd ${local}
--- comps/data_preview.py
+++ comps/data_preview.py
@@ -4,9 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import netcdf
 from scipy import interpolate
+import matplotlib.animation as animation
 
-wrfdata = '/Users/nadya2/data/plume/comps/part1/wrfout_2'
-vert_levels = np.arange(0,3000,100)
+
+wrfdata = '/Users/nadya2/data/plume/comps/part1/wrfout_3'
+vert_levels = np.arange(0,1500,50)
+wrfinterp = '/Users/nadya2/code/plume/comps/interpT.nc'
 
 # vars_4d = ['U','V','QVAPOR']
 # vars_3d = ['HGT','GRNHFX','AVG_FUEL_FRAC','GRNQFX']
@@ -24,27 +27,66 @@ nc_data = netcdf.netcdf_file(wrfdata, mode ='r')
 #     print('......... %s' %var3)
 #     fc_data[var3] = nc_data.variables[var3][:,:,:]
 
-
+# # ===========vertical interpolation done in ncl===================
 #add level height data
 print('Calculating vertical level heights for the domain')
-nc_data.createVariable('LVLHGT', float, ('Time', 'bottom_top_stag', 'south_north', 'west_east'))
+# nc_data.createVariable('LVLHGT', float, ('Time', 'bottom_top_stag', 'south_north', 'west_east'))
 lvlhgt = (nc_data.variables['PH'][:,:,:,:] + nc_data.variables['PHB'][:,:,:,:]) / 9.81
 nc_data.variables['LVLHGT'] = lvlhgt
 dims = np.shape(lvlhgt)
 
 
-#interpolate to vertical height levels
-print('Performing vertical interpolation of model data')
-T_agl = np.empty((dims[0],len(vert_levels),dims[2],dims[3]))
-for nTime in range(dims[0]):
-	print('.....timestep since simulation start: %smin' %nc_data.variables['XTIME'][nTime])
-	for nY in range(dims[2]):
-		for nX in range(dims[3]):
-			eta =  lvlhgt[nTime,:-1,nY,nX]
-			interp_func = interpolate.interp1d(eta, nc_data.variables['T'][nTime,:,nY,nX],\
-						 'cubic', bounds_error=False, fill_value=np.nan)
-			T_agl[nTime,:,nY,nX] = interp_func(vert_levels)
+# # #interpolate to vertical height levels
+# # print('Performing vertical interpolation of model data')
+# # T_agl = np.empty((dims[0],len(vert_levels),dims[2],dims[3]))
+# # for nTime in range(dims[0]):
+# # 	print('.....timestep since simulation start: %smin' %nc_data.variables['XTIME'][nTime])
+# # 	for nY in range(dims[2]):
+# # 		for nX in range(dims[3]):
+# # 			eta =  lvlhgt[nTime,:-1,nY,nX]
+# # 			interp_func = interpolate.interp1d(eta, nc_data.variables['T'][nTime,:,nY,nX],\
+# # 						 'cubic', bounds_error=False, fill_value=np.nan)
+# # 			T_agl[nTime,:,nY,nX] = interp_func(vert_levels)
+# #===========vertical interpolation done in ncl===================
 
-# nc_data.close()
-plt.pcolormesh(T_agl[120,:,75,:])
+ttest= 170
+
+print("plotting....")
+plt.figure()
+plt.pcolormesh(nc_data.variables['T'][ttest,0:20,75,:]+300,vmin=300, vmax=310)
+plt.colorbar()
+plt.show()
+plt.close()
+
+plt.figure()
+nc_interp_data = netcdf.netcdf_file(wrfinterp, mode ='r') 
+interpT = np.copy(nc_interp_data.variables['T'][:,:,:,:])
+interpT[interpT>100] = np.nan
+plt.pcolormesh(interpT[ttest,:,75,:]+300,vmin=300, vmax=310)
+plt.colorbar()
+plt.show()
+plt.close()
+
+
+#sanity check: plot vertical column of temperatures for raw and interp dataset
+plt.figure()
+plt.subplot(2,1,1)
+plt.plot(lvlhgt[ttest,:23,75,75],nc_data.variables['T'][ttest,:23,75,75]+300,'k')
+plt.subplot(2,1,2)
+plt.plot(np.arange(0,1.5,0.02)[3:]*1000,interpT[ttest,:,75,75][3:]+300,'r')
+plt.show()
+plt.close()
+
+
+#animation of BL growth
+fig = plt.figure()
+ims = []
+for i in range(len(interpT[:,0,75,0])):
+    t_step = int(i)
+    im = plt.pcolormesh(interpT[i,:,75,:]+300, vmin=300, vmax=310)
+    ims.append([im])
+
+ani = animation.ArtistAnimation(fig, ims, interval=120, blit=False,repeat_delay=1000)
+plt.colorbar()
+
 plt.show()
