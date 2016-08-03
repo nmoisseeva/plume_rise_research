881a6e107256a34a1b660c5500660d62952af982
Nadya Moisseeva <moisseev@seawolf3.ibb>
2016-08-03 10:36:35 -0700

    westgrid - back to looped runs

--- westgrid/westgrid_comps_wrapper.bash
+++ westgrid/westgrid_comps_wrapper.bash
@@ -8,8 +8,8 @@
 #input-----------------------------
 part="B"
 section=(a b c)
-input_dir="~/plume/comps/"
-data_dir="~/plume/comps/westgrid/"
+input_dir="/home/moisseev/plume/comps/"
+data_dir="/home/moisseev/plume/comps/westgrid/"
 
 #----------------------------------
 
@@ -29,24 +29,24 @@ do
     cd $WRFRUNDIR
 
     #clean up original settings files
-    rm input_sounding
-    rm namelist.input
-    rm namelist.fire
+    rm ${WRFRUNDIR}input_sounding
+    rm ${WRFRUNDIR}namelist.input
+    rm ${WRFRUNDIR}namelist.fire
 
     echo "..... subsection $nRun"
     #symlink the proper setting files
-    ln -s ${input_dir}${part}/namelist.fire ./
-    ln -s ${input_dir}${part}/namelist.input ./
-    ln -s ${input_dir}${part}/input\_sounding\_${nRun} ./input\_sounding
+    ln -s ${input_dir}${part}/namelist.fire $WRFRUNDIR
+    ln -s ${input_dir}${part}/namelist.input $WRFRUNDIR
+    ln -s ${input_dir}${part}/input\_sounding\_${nRun} ${WRFRUNDIR}input\_sounding
     echo "Running WRF for case: $nRun"
-    ./ideal.exe
+    ${WRFRUNDIR}ideal.exe
     echo ".....IDEAL.EXE complete"
-    ./wrf.exe 
+    ${WRFRUNDIR}wrf.exe 
     echo ".....WRF.EXE complete: $date"
 
     #rename and move the results to a data directory
     mkdir -p ${data_dir}${part}/ 
-    mv wrfout\_d01\_0000\-01\-01\_00:00:00 ${data_dir}${part}/wrfout\_$nRun
+    mv ${WRFRUNDIR}wrfout\_d01\_0000\-01\-01\_00:00:00 ${data_dir}${part}/wrfout\_$nRun
 
     ##run interpolation code
     #echo "Running NCL for data interpolation: $nRun"
@@ -54,7 +54,7 @@ do
     #mkdir -p interp
     #ln -s  ${data_dir}${part}/wrfout\_$nRun ./interp/wrfout\_temp
     #ncl63 data_prep.ncl 
-    #mv intperp\_wrf.nc ./intperp/intperp\_wrf\_${nRun}
+    #mv `intperp\_wrf.nc ./intperp/intperp\_wrf\_${nRun}
     
 done
 
