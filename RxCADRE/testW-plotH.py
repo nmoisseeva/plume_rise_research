88b2124c1d9012bc58c3e472237b318d8d421e46
Nadya Moisseeva <nmoisseeva@a23-218.eos.ubc.ca>
2019-08-08 16:41:32 -0700

    vertical wind analysis, figure saving

--- RxCADRE/testW-plotH.py
+++ RxCADRE/testW-plotH.py
@@ -11,21 +11,21 @@ import imp
 from datetime import datetime, timezone, timedelta
 import matplotlib.dates as mdates
 import pytz
+import os.path
+
 
 #====================INPUT===================
 
 #all common variables are stored separately
-import rxcadreDRY as rx
+import rxcadreMOIST as rx
 imp.reload(rx) 	            #force load each time
 
 
 datapath = './csv/W-H_micromet1Hz.csv'
 
 freq = 1 		     #hz of wind measurements
-ave_int_H = 300 	 #seconds
+ave_int_H = 300 	 #seconds          #currently NOT USED!
 ave_int_W = 60       #seconds
-
-
 
 hfx_sensor_conversion = 0.170           #mV m^2/kW
 hfx_sensor_height = 2.8                 #meters AGL
@@ -47,18 +47,23 @@ UTMy = nc_data.variables['XLAT'][0,:,:] + rx.ll_utm[1]
 
 ncdict = wrf.extract_vars(nc_data, None, ('PHB','PH','W'))
 
-#get height and destagger vars
-print('...destaggering data')
-zstag = (ncdict['PHB'] + ncdict['PH'])/ 9.81
-z = wrf.destagger(zstag,1)
+#get height and destagger
+if os.path.isfile(rx.z_path):
+    print('.....loading destaggered height array from %s' %rx.z_path)
+    z = np.load(rx.z_path)
+else:
+    print('.....destaggering height data')
+    zstag = (ncdict['PHB'] + ncdict['PH'])/ 9.81
+    z = wrf.destagger(zstag,1)
+    np.save(rx.z_path, z)
+
+print('.....destaggering model W')
 w = wrf.destagger(ncdict['W'],1)
 
-nT,nZ,nY,nX = np.shape(zstag)
-
-#--------------NOT TESTED FROM HERE OWN-------------------
+nT,nZ,nY,nX = np.shape(z)
 
 #create timeseries of micromet-tower wind
-print('-->Creating OBS timeries')
+print('.....creating OBS timeries')
 num_pts = int(freq * ave_int_W)
 data_samples = int(np.shape(obs_data)[0] / num_pts)
 wMET = []
@@ -74,12 +79,12 @@ for nSample in range(data_samples):
 
 
 #get grid location of where CSU is
-print('-->Finding closest model location')
+print('.....finding closest model location')
 grid_coord_atm = np.array(list(zip(UTMy.ravel(),UTMx.ravel())))
 gridTree = KDTree(grid_coord_atm)
 METdist, METgrid_id = gridTree.query([rx.met_lcn[1], rx.met_lcn[0]])
 METidx = np.unravel_index(METgrid_id,np.shape(UTMx))
-#--------------NOT TESTED FROM HERE OWN-------------------
+
 
 #get windspeed and height vector at the micromet tower location location
 wrf_W = w[:,:,METidx[0],METidx[1]]
@@ -95,14 +100,14 @@ for nSample in range(rx.run_min):
 
 
 #=======================PLOTTING=======================
-plt.title('MICROMET vs WRF  Vertical Velocity w ')
-plt.plot(subset_time[-rx.run_min:],np.array(wWRF)[:,0], label='WRF-SFIRE 8m')
-plt.plot(subset_time[-rx.run_min:],np.array(wMET)[-rx.run_min:], label='Micromet tower winds 5.8m')
+plt.title('IN-SITU vs WRFSFIRE W')
+plt.plot(subset_time[-rx.run_min:],np.array(wWRF)[:,0], label='WRF-SFIRE (8 m)')
+plt.plot(subset_time[-rx.run_min:],np.array(wMET)[-rx.run_min:], label='In-situ tower (5.8 m)')
 ax = plt.gca()
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
 plt.gcf().autofmt_xdate()
 plt.xlabel('time (CST)')
-plt.ylabel('wind speed [m/s]')
+plt.ylabel('vertical wind speed [m/s]')
 plt.legend()
 plt.show()
 plt.close()
@@ -115,5 +120,6 @@ plt.gcf().autofmt_xdate()
 plt.xlabel('time (CST)')
 plt.ylabel('heat flux (W/m2)')
 plt.legend()
+plt.savefig(rx.fig_dir + 'VerticalWind.pdf')
 plt.show()
 plt.close()
