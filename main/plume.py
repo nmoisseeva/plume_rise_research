15dcec73f216d38b020db6fe44f1af94ddd3fd9f
Nadya Moisseeva <nmoisseeva@eos.ubc.ca>
2020-05-19 16:35:40 -0700

    as of addition of R5 profile

--- main/plume.py
+++ main/plume.py
@@ -25,7 +25,7 @@ dirlist = glob.glob(dirpath)                        #get all  interp files in di
 tag = [i[len(dirpath)-1:-4] for i in dirlist]       #W*F*R0
 # tag = ['W8S400F7R0']
 # exclude_runs = ['W5F4R0','W5F4R1','W5F4R2','W5F4R3','W5F4R4' ]
-exclude_runs = ['W5F4R0','W5F4R1','W5F4R2','W5F4R3','W5F4R4' ,'W5F8R3','W5F9R3','W5F1R3','W5F13R0']
+exclude_runs = ['W5F4R0','W5F8R3','W5F9R3','W5F1R3','W5F13R0']
 
 
 # exclude_runs = []
