360331c28c3999c910e4b937d8edd32125774f85
Nadya Moisseeva <nmoisseeva@eos.ubc.ca>
2020-09-30 18:14:01 -0700

    wokring lateral dispersion - supervisory submission

--- main/disperse.py
+++ main/disperse.py
@@ -66,24 +66,24 @@ for nCase,Case in enumerate(RunList):
 
         fig = plt.figure(figsize=(6,5))
         plt.subplot(211)
-        plt.title('(a) ALONG-WIND TOTAL SMOKE')
+        plt.title('(a) AWI SMOKE')
         im = plt.imshow(NlateralPM, origin='lower', extent=[0,yaxis[-1],0,pmlvl[-1]],cmap=plt.cm.cubehelix_r,vmin=0,vmax = 1)
         plt.colorbar(im, label=r'normalized concentration')
         plt.axhline(y = zCL,ls='--', c='dimgrey',label=r'$z_{CL}$' )
-        plt.gca().set(ylim=[0,pmlvl[-1]],aspect='equal',ylabel='height [m]',xlabel='distance [m]')
+        plt.gca().set(ylim=[0,pmlvl[-1]],aspect='equal',ylabel='height [m]',xlabel='cross-wind distance [m]')
         plt.legend()
         plt.subplot(212)
         plt.title(r'(b) CROSSSECTION AT $z_{CL}$')
         plt.plot(yaxis,NcrosszCL)
-        plt.gca().set(ylabel=r'normalized concentration',xlabel='distance [m]')
+        plt.gca().set(ylabel=r'normalized concentration',xlabel='cross-wind distance [m]')
         plt.tight_layout()
         # plt.show()
         plt.savefig(plume.figdir + 'lateral/lat%s.pdf' %Case )
         plt.close()
 
         #get plume edges
-        edge1 = np.nanargmin(abs(NcrosszCL[:int(dimY/2)] - 0.21)) * plume.dy
-        edge2 = (np.nanargmin(abs(NcrosszCL[int(dimY/2):] - 0.21)) + int(dimY/2))* plume.dy
+        edge1 = np.nanargmin(abs(NcrosszCL[:int(dimY/2)] - 0.025)) * plume.dy
+        edge2 = (np.nanargmin(abs(NcrosszCL[int(dimY/2):] - 0.025)) + int(dimY/2))* plume.dy
         dist1 = 4000 - edge1
         dist2 = edge2 - 6000
         dropoff[nCase] = np.mean([dist1,dist2])
@@ -91,8 +91,7 @@ for nCase,Case in enumerate(RunList):
         # wf[nCase] = Phi[nCase]/(plumes['zi'][runNumber] * plumes['Omega'][runNumber])
         wf[nCase] = (9.81 * Phi[nCase] * (plumes['zCL'][runNumber] - 0.75 * plumes['zi'][runNumber] )/ (plumes['zi'][runNumber] * plumes['thetaS'][runNumber]))**(1/3.)
 
-        span[nCase] = np.mean([edge2, edge1])
-plt.figure()
+        span[nCase] = (edge2-edge1)/2.
 xaxis = np.arange(0,np.nanmax(Phi))
 plt.title('PLUME WIDENING')
 plt.scatter(Phi,dropoff,c=plume.read_tag('F',RunList))
@@ -120,8 +119,8 @@ runCnt = len(RunList)                           #count number of cases
 
 #loop through all LES cases
 plt.figure(figsize = (6,10))
-plt.subplot(3,1,1)
-plt.title(r'(a) FIRELINE LENGTH EFFECT')
+plt.subplot(3,1,2)
+plt.title(r'(b) FIRELINE LENGTH EFFECT')
 for nCase,Case in enumerate(RunList):
     print('...case: %s' %Case)
     #create an interpolated profile of temperature
@@ -155,18 +154,18 @@ for nCase,Case in enumerate(RunList):
 
     #modelling class
     sigma = 1000+PhiLoc*(2500)/(140000)
-    sigmaG = (500*wf[runNumber] + 2000)
+    sigmaG = (fitWf[0]*wf[runNumber] + fitWf[1])/2
     modelledCS = np.empty_like(NcrosszCL) * np.nan
     s, f = edgeDist[nCase], (dimY-edgeDist[nCase])
     modelledCS[s:f] =0.9
     modelledCS[:s] =0.9* np.exp(-0.5*((yaxis[:s] - s*plume.dy)/sigma)**2)
     modelledCS[f:] =0.9* np.exp(-0.5*((yaxis[f:] - f*plume.dy)/sigma)**2)
     GaussianCS = np.exp((-0.5*((yaxis - 5000)/sigmaG)**2))
-    plt.plot(yaxis,GaussianCS, ls='--',c=clr[nCase])
+    plt.plot(yaxis,GaussianCS, ls=':',c=clr[nCase])
 
     plt.plot(yaxis,NcrosszCL,label=RunLbl[nCase])
     # plt.plot(yaxis,modelledCS, ls=':',c=clr[nCase])
-    plt.gca().set(ylabel=r'normalized concentration',xlabel='distance [m]')
+    plt.gca().set(ylabel=r'normalized concentration',xlabel='cross-wind distance [m]')
     plt.legend()
 
 
@@ -179,8 +178,8 @@ runCnt = len(RunList)                           #count number of cases
 edgeDist = 100
 
 #loop through all LES cases
-plt.subplot(3,1,2)
-plt.title(r'(b) AMBIENT WIND EFFECT')
+plt.subplot(3,1,3)
+plt.title(r'(c) AMBIENT WIND EFFECT')
 for nCase,Case in enumerate(RunList):
     print('...case: %s' %Case)
     #create an interpolated profile of temperature
@@ -213,33 +212,33 @@ for nCase,Case in enumerate(RunList):
 
     #modelling class
     sigma = 1000+PhiLoc*(2500)/(140000)
-    sigmaG = (500*wf[runNumber] + 2000)
+    sigmaG = (fitWf[0]*wf[runNumber] + fitWf[1])/2
     modelledCS = np.empty_like(NcrosszCL) * np.nan
     s, f = edgeDist, (dimY-edgeDist)
     modelledCS[s:f] = 0.9
     modelledCS[:s] = 0.9*np.exp(-0.5*((yaxis[:s] - s*plume.dy)/sigma)**2)
     modelledCS[f:] = 0.9*np.exp(-0.5*((yaxis[f:] - f*plume.dy)/sigma)**2)
     GaussianCS = np.exp((-0.5*((yaxis - 5000)/sigmaG)**2))
-    plt.plot(yaxis,GaussianCS, ls='--',c=clr[nCase])
+    plt.plot(yaxis,GaussianCS, ls=':',c=clr[nCase])
 
     # plt.plot(yaxis,modelledCS, ls=':',c=clr[nCase])
     plt.plot(yaxis,NcrosszCL,label=RunLbl[nCase])
 
 
-    plt.gca().set(ylabel=r'normalized concentration',xlabel='distance [m]')
+    plt.gca().set(ylabel=r'normalized concentration',xlabel='cross-wind distance [m]')
     plt.legend()
 
 
 #======================compare conditions:Intensity===================
 
 #length tests
-RunList =   ['W5F1R1','W5F6R1']
+RunList =   ['W5F8R2','W5F6R2']
 RunLbl =   ['low intensity','high intensity']
 runCnt = len(RunList)                           #count number of cases
 
 #loop through all LES cases
-plt.subplot(3,1,3)
-plt.title(r'(c) FIRELINE HEAT EFFECT')
+plt.subplot(3,1,1)
+plt.title(r'(a) FIRELINE HEAT EFFECT')
 for nCase,Case in enumerate(RunList):
     print('...case: %s' %Case)
     #create an interpolated profile of temperature
@@ -272,18 +271,19 @@ for nCase,Case in enumerate(RunList):
 
     #modelling class
     sigma = 1000+PhiLoc*(2500)/(140000)
-    sigmaG = (500*wf[runNumber] + 2000)
+    sigmaG = (fitWf[0]*wf[runNumber] + fitWf[1])/2
+    print(sigmaG)
     modelledCS = np.empty_like(NcrosszCL) * np.nan
     s, f = edgeDist, (dimY-edgeDist)
     modelledCS[s:f] = 0.9
     modelledCS[:s] = 0.9*np.exp(-0.5*((yaxis[:s] - s*plume.dy)/sigma)**2)
     modelledCS[f:] = 0.9*np.exp(-0.5*((yaxis[f:] - f*plume.dy)/sigma)**2)
     GaussianCS = np.exp((-0.5*((yaxis - 5000)/sigmaG)**2))
-    plt.plot(yaxis,GaussianCS, ls='--',c=clr[nCase])
+    plt.plot(yaxis,GaussianCS, ls=':',c=clr[nCase])
 
     plt.plot(yaxis,NcrosszCL,label=RunLbl[nCase])
     # plt.plot(yaxis,modelledCS, ls=':',c=clr[nCase])
-    plt.gca().set(ylabel=r'normalized concentration',xlabel='distance [m]')
+    plt.gca().set(ylabel=r'normalized concentration',xlabel='cross-wind distance [m]')
     plt.legend()
 
 # plt.show()
